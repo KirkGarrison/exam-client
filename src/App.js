b1b372391fc41b28e42bf85df098da8060578d88
sara russert <sararussert@saras-MacBook-Pro-2.local>
2021-11-22 14:54:53 -0800

    last update

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ export default class App extends Component {
       characters: []
     }
   }
-  // write a route that gets characters from the server and then somehow renders them to the screen... see Characters component for ideas on how to render
-  // write a route that allows you to add a brand new character of your creation to your list of characters - use the NewCharacterForm component to help facilitate that either by putting it in a modal or by using a router
-  // write a route that allows you to update a character with notes
-  // write a route that allows you to delete characters
+  // TODO: write a function that make a request to get characters from the server and then somehow renders them to the screen... see Characters component for ideas on how to render
+  // TODO: write a function that make a request to post, allows you to add a brand new character of your creation to your list of characters - use the NewCharacterForm component to help facilitate that either by putting it in a modal or by using a router
+  // TODO: write a function that make a request to put, to update a character with notes
+  // TODO: write a function that make a request to delete characters
+
   render() {
     return (
       <div>
